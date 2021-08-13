01270cc101b40e143c07e623dc480ae71efe5f8b
Bruno Ramos Oliveira <venomplay@hotmail.com>
2021-08-12 22:09:03 -0300

    test

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -17,20 +17,20 @@ import static org.hamcrest.Matchers.contains;
 // 3 - Class
 public class Pet {
     // 3.1 - Atributos
-    String uri = "https://petstore.swagger.io/v2/pet"; // endereÃ§o da entidade Pet
+    String uri = "https://petstore.swagger.io/v2/pet"; // endereço da entidade Pet
 
-    // 3.2 - MÃ©todos e FunÃ§Ãµes
+    // 3.2 - Métodos e Funções
     public String lerJson(String caminhoJson) throws IOException {
         return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
 
     // Incluir - Create - Post
-    @Test // Identifica o mÃ©todo ou funÃ§Ã£o como um teste para o TestNG
+    @Test(priority = 1) // Identifica o método ou função como um teste para o TestNG
     public void incluirPet() throws IOException {
         String jsonBody = lerJson("db/pet1.json");
 
         // Sintaxe Gherkin
-        // Dado - Quando - EntÃ£o
+        // Dado - Quando - Então
         // Given - When - Then
 
         given() // Dado
@@ -44,10 +44,69 @@ public class Pet {
                 .statusCode(200)
                 .body("name", is("Branca"))
                 .body("status", is("available"))
-                .body("category.name", is("dog"))
-                .body("tags.name", contains("sta"))
+                .body("category.name", is("AX2345LORT"))
+                .body("tags.name", contains("data"))
         ;
 
     }
+    @Test(priority = 2)
+    public void consultarPet(){
+        String petId = "961349455";
+
+        String token =
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .get(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Branca"))
+                .body("category.name", is("AX2345LORT"))
+                .body("status", is("available"))
+        .extract()
+                .path("category.name")
+    ;
+                System.out.println("o token é " + token);
+
+    }
+    @Test(priority = 3)
+    public void alterarPet() throws IOException {
+        String jsonBody = lerJson("db/pet2.json");
+
+        given()
+                .contentType("application/json")
+                .log().all()
+                .body(jsonBody)
+
+        .when()
+                .put(uri)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Branca"))
+                .body("status", is("sold"))
+        ;
+    }
+    @Test(priority = 4)
+    public void excluirPet() throws IOException {
+        String petId = "961349455";
+
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .delete(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("type", is("unknown"))
+                .body("message", is("961349455"))
+
+        ;
+    }
+
 
 }
